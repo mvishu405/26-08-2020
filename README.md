1411681dd3cd7e8d30111b0aa625a0829bb8372d
mvishu405 <mvishu405@gmail.com>
2020-08-26 19:23:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-4jhkzq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-4jhkzq)
---
+++ index.js
@@ -0,0 +1,30 @@
+let boolObj = new Boolean(false);
+
+let result = 1 && 2 && 3 && 4 ;
+
+console.log(result);
+
+
+let a = String.fromCharCode('Hello U+1F600');
+console.log(a);
+
+
+let message = 'Hello ';
+console.log(message.length);
+
+console.log(String.fromCharCode(97, 98, 55357, 56842, 100, 101));
+
+
+
+
+let stringValue = "foo";
+
+console.log(stringValue.padStart(600)); // " foo"
+// console.log(stringValue.padStart(9, ".")); // "......foo"
+// console.log(stringValue.padEnd(6)); // "foo "
+// console.log(stringValue.padEnd(9, ".")); // "foo......"
+
+console.log(stringValue.anchor("vishal"));
+
+
+
